6b3b4ebefb33cc383514038e7ef5c0f883aad442
HWZen <1041453977@qq.com>
2022-06-16 23:04:23 +0800

    update listSample.cpp

--- sample/listSample/listSample.cpp
+++ sample/listSample/listSample.cpp
@@ -3,9 +3,21 @@
 //
 #include "io.h"
 #include "list.h"
+using namespace sstd;
 int main(){
-    using namespace sstd;
+
     listInt l1({1,2,3,4,5});
+    Println("l1.size() = ", l1.size());
+    for(auto &it:l1)
+        Print(it, " ");
     listInt l2(std::move(l1));
-    print(l2.size());
+    Println(l2.size());
+    Println("l2.front() = ", l2.front());
+    Println("l2.back() = ", l2.back());
+    l2.push_back(100);
+    Println("l2.back() = ", l2.back());
+    auto it = l2.begin();
+    l2.pop(it);
+    Println("l2.size() = ", l2.size());
+    Println("l2.front() = ", l2.front());
 }

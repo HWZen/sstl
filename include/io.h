016f731f71657500d4c0186a24b11200077f25da
HWZen <1041453977@qq.com>
2022-06-16 23:04:03 +0800

    update io.h and add ioSample.cpp

--- include/io.h
+++ include/io.h
@@ -12,23 +12,38 @@
 #endif
 #include <corecrt_io.h>
 #endif
-#include "fast_io/include/fast_io.h"
+
 #include <utility>
+#include <type_traits>
+
+#include "fast_io/include/fast_io.h"
+
+#define CONSTEXPR inline constexpr
 
 namespace sstd{
-    void Print(auto &&... args){
-        print(std::forward<decltype(args)>(args)...);
-    }
-    void Println(auto &&... args){
-        println(std::forward<decltype(args)>(args)...);
+
+    CONSTEXPR void Println(){
+        ::print("\n");
     }
 
-    void print(auto &&... args){
-        Print(std::forward<decltype(args)>(args)...);
+    CONSTEXPR void Print(){}
+
+    CONSTEXPR void Print(auto &&val ,auto &&... args){
+        if constexpr (std::is_same_v<std::remove_reference_t<decltype((val))>, const char *>){
+            ::print(fast_io::mnp::os_c_str(val));
+        }
+        else
+            ::print(std::forward<decltype(val)>(val));
+        sstd::Print(args...);
     }
 
-    void println(auto &&... args){
-        Println(std::forward<decltype(args)>(args)...);
+    CONSTEXPR void Println(auto &&val, auto &&... args){
+        if constexpr (std::is_same_v<std::remove_reference_t<decltype((val))>, const char *>){
+            ::print(fast_io::mnp::os_c_str(val));
+        }
+        else
+            ::print(std::forward<decltype(val)>(val));
+        sstd::Println(args...);
     }
 
     class _Out{
@@ -39,15 +54,35 @@ namespace sstd{
         _Out& operator=(_Out&&) = delete;
         ~_Out() = default;
     public:
-        static _Out& out(){
+        static _Out& Out(){
             static _Out instance;
             return instance;
         }
-        _Out& operator<<(auto&& arg){
+        const _Out& operator<<(auto&& arg) const{
             print(std::forward<decltype(arg)>(arg));
             return *this;
         }
     };
+
+    inline const _Out& out = _Out::Out();
+
+    inline auto get_time(){
+        return fast_io::posix_clock_gettime(fast_io::posix_clock_id::realtime);
+    }
+
+    inline auto get_utc_time(){
+        return utc(get_time());
+    }
+
+    inline auto get_local_time(){
+        return local(get_time());
+    }
+
+    inline const auto TIMEZONE{[](){
+        fast_io::posix_tzset();
+        return fast_io::timezone_name();
+    }()};
+
 }
 
 
---
+++ sample/ioSample/ioSample.cpp
@@ -0,0 +1,20 @@
+//
+// Created by HWZen on 2022/6/16.
+// Copyright (c) 2022 HWZen All rights reserved.
+// MIT License
+//
+
+#include <io.h>
+
+int main(){
+    using namespace sstd;
+    Print("Hello, World!\n");
+    Println("Hello, World!");
+    Println("Hello, Now local time:  ", get_local_time());
+    Println("Hello, Now utc time:  ", get_utc_time());
+    Println("Hello, Now ", get_time(), "secs has passed since 1990-01-01 00:00:00");
+    Println("Hello, Your timezone: ", TIMEZONE);
+    println(1," ", 2, " ", 3, " ", 4, " ", 5, " ", 3.14159, " Hello world ", 0xfffffffff);
+    out << "Hello, World!" << 1 << 2 << 3 << 4 << 5 << 3.14159 << 0xfffffffff << "\n";
+}
+

6f62da9403fce1c16fb2c46db9375526443c28e6
HWZen <1041453977@qq.com>
2022-07-06 21:11:08 +0800

    rebuild atomic_queue.h

--- include/atomic_queue.h
+++ include/atomic_queue.h
@@ -7,25 +7,26 @@
 #ifndef SSTL_ATOMIC_QUEUE_H
 #define SSTL_ATOMIC_QUEUE_H
 
-#include <array.h>
+#include <queue>
 #include <semaphore>
 #include <mutex>
 #include <optional>
+#include <atomic>
+#include "functional.h"
 
 namespace sstd{
-    // TODO:add m_size to function, consider m_front and m_back is need mutex.
-    template<typename T, size_t queue_size>
+    template<typename T>
     class atomic_queue{
     public:
         using value_type = T;
         using pVal = value_type*;
         using refVal = value_type&;
 
-        constexpr atomic_queue();
+        constexpr atomic_queue() = default;
+
+        constexpr atomic_queue(atomic_queue&&) noexcept;
 
         constexpr atomic_queue(const atomic_queue &) = delete;
-
-        constexpr atomic_queue(atomic_queue&&)  noexcept = default;
 
         ~atomic_queue() = default;
 
@@ -36,14 +37,14 @@ namespace sstd{
         constexpr value_type& back();
 
         // warning: Block indefinitely
-        constexpr void push( weak_same_type<value_type> auto &&data);
+        constexpr void push(auto &&data);
 
         // warning: Block indefinitely
         constexpr value_type pop();
 
-        constexpr std::optional<value_type&> try_front();
+        constexpr std::optional<value_type> try_front();
 
-        constexpr std::optional<value_type&> try_back();
+        constexpr std::optional<value_type> try_back();
 
         constexpr bool try_push(weak_same_type<value_type> auto &&data);
 
@@ -51,10 +52,10 @@ namespace sstd{
         constexpr std::optional<value_type> try_pop();
 
         template <class _Rep, class _Period>
-        constexpr std::optional<value_type&> try_front_for(const std::chrono::duration<_Rep, _Period>);
+        constexpr std::optional<value_type> try_front_for(const std::chrono::duration<_Rep, _Period>);
 
         template <class _Rep, class _Period>
-        constexpr std::optional<value_type&> try_back_for(const std::chrono::duration<_Rep, _Period>);
+        constexpr std::optional<value_type> try_back_for(const std::chrono::duration<_Rep, _Period>);
 
         template <class _Rep, class _Period>
         constexpr bool try_push_for(weak_same_type<value_type> auto &&data, const std::chrono::duration<_Rep, _Period>);
@@ -62,91 +63,172 @@ namespace sstd{
         template <class _Rep, class _Period>
         constexpr std::optional<value_type> try_pop_for(const std::chrono::duration<_Rep, _Period>);
 
-        constexpr size_t size() const;
+        constexpr size_t size() {
+            return m_data.size();
+        }
+
+        constexpr size_t size() const{
+            return m_data.size();
+        }
 
     private:
-        array<value_type , queue_size> m_data;
-        size_t m_front{0};
-        size_t m_back{0};
-        size_t m_size{0};
-        std::unique_lock<std::mutex> m_producer_lock;
-        std::unique_lock<std::mutex> m_consumer_lock;
+        std::queue<value_type> m_data;
+        std::binary_semaphore m_producer_lock{1};
+        std::binary_semaphore m_consumer_lock{0};
     };
 
-    template<typename T, size_t queue_size>
-    constexpr atomic_queue<T, queue_size>::atomic_queue() {
-        m_consumer_lock.lock();
-    }
+    template<typename T>
+    constexpr atomic_queue<T>::atomic_queue(atomic_queue &&other) noexcept : m_data{std::move(other.m_data)} {}
 
-    template<typename T, size_t queue_size>
-    constexpr T &atomic_queue<T, queue_size>::front() {
-        m_consumer_lock.lock();
-        auto &res = m_data[m_front];
-        m_consumer_lock.unlock();
+    template<typename T>
+    constexpr T &atomic_queue<T>::front() {
+        m_consumer_lock.acquire();
+        auto &res = m_data.front();
+        m_consumer_lock.release();
         return res;
     }
 
-    template<typename T, size_t queue_size>
-    constexpr T &atomic_queue<T, queue_size>::back() {
-        m_consumer_lock.lock();
-        auto &res = m_data[(m_back - 1) < 0 ? queue_size - 1 : m_back - 1];
-        m_consumer_lock.unlock();
+    template<typename T>
+    constexpr T &atomic_queue<T>::back() {
+        m_consumer_lock.acquire();
+        auto &res = m_data.back();
+        m_consumer_lock.release();
         return res;
     }
 
-    template<typename T, size_t queue_size>
-    constexpr void atomic_queue<T, queue_size>::push(weak_same_type<value_type> auto &&data) {
-        m_producer_lock.lock();
-        m_data[m_back] = std::forward(data);
-        m_back = (m_back + 1 >= queue_size) ? 0 : m_back + 1;
-        if(size() == 1)
-            m_consumer_lock.unlock();
-        if(size() < queue_size - 1)
-            m_producer_lock.unlock();
+    template<typename T>
+    constexpr void atomic_queue<T>::push(auto &&data) {
+        m_producer_lock.acquire();
+        if(!m_data.empty())
+            m_consumer_lock.acquire();
+        m_data.emplace(data);
+        m_consumer_lock.release();
+        m_producer_lock.release();
     }
 
-    template<typename T, size_t queue_size>
-    constexpr T atomic_queue<T, queue_size>::pop() {
-        m_consumer_lock.lock();
-        auto res = std::move(m_data[m_front]);
-        m_front = (m_front + 1 >= queue_size) ? 0 : m_front + 1;
-        if(size() == queue_size - 1)
-        if(size() > 0)
-            m_consumer_lock.unlock();
+    template<typename T>
+    constexpr T atomic_queue<T>::pop() {
+        m_producer_lock.acquire();
+        m_consumer_lock.acquire();
+        auto res = std::move(m_data.front());
+        m_data.pop();
+        if(!m_data.empty())
+            m_consumer_lock.release();
+        m_producer_lock.release();
         return res;
     }
 
-    template<typename T, size_t queue_size>
-    constexpr std::optional<T &> atomic_queue<T, queue_size>::try_front() {
-        if(m_consumer_lock.try_lock()){
-            auto &res = m_data[m_front];
-            m_consumer_lock.unlock();
+    template<typename T>
+    constexpr std::optional<T> atomic_queue<T>::try_front() {
+        if(m_consumer_lock.try_acquire()){
+            auto &res = m_data.front();
+            m_consumer_lock.release();
             return {res};
         }
         else
             return {};
     }
 
-    template<typename T, size_t queue_size>
-    constexpr std::optional<T &> atomic_queue<T, queue_size>::try_back() {
-        if(m_consumer_lock.try_lock()){
-            auto &res = m_data[(m_back - 1) < 0 ? queue_size - 1 : m_back - 1];
-            m_consumer_lock.unlock();
+    template<typename T>
+    constexpr std::optional<T> atomic_queue<T>::try_back() {
+        if(m_consumer_lock.try_acquire()){
+            auto &res = m_data.back();
+            m_consumer_lock.release();
             return {res};
         }
         else
             return {};
     }
 
-    template<typename T, size_t queue_size>
-    constexpr bool atomic_queue<T, queue_size>::try_push(weak_same_type<value_type> auto &&data) {
-        if(m_producer_lock.try_lock()){
-            m_data[m_back] = std::forward(data);
-            m_data = (m_data + 1 >= queue_size) ? 0 : m_data + 1;
-            m_producer_lock.unlock();
-            return true;
-        }else
+    template<typename T>
+    constexpr bool atomic_queue<T>::try_push(weak_same_type<value_type> auto &&data) {
+        if(!m_producer_lock.try_acquire())
             return false;
+        if(!m_data.empty() && !m_consumer_lock.try_acquire()){
+            m_producer_lock.release();
+            return false;
+        }
+
+        m_data.push(data);
+        m_consumer_lock.release();
+        m_producer_lock.release();
+        return true;
+    }
+
+    template<typename T>
+    constexpr std::optional<T> atomic_queue<T>::try_pop() {
+        if(!m_producer_lock.try_acquire())
+            return {};
+        if(!m_consumer_lock.try_acquire()){
+            m_producer_lock.release();
+            return {};
+        }
+
+        std::optional<T> res{std::move(m_data.front())};
+        m_data.pop();
+        if(!m_data.empty())
+            m_consumer_lock.release();
+        m_producer_lock.release();
+        return res;
+    }
+
+    template<typename T>
+    template<class _Rep, class _Period>
+    constexpr std::optional<T> atomic_queue<T>::try_front_for(const std::chrono::duration<_Rep, _Period> times) {
+        if(m_consumer_lock.try_acquire_for(times)){
+            auto &res = m_data.front();
+            m_consumer_lock.release();
+            return {res};
+        }
+        else
+            return {};
+    }
+
+    template<typename T>
+    template<class _Rep, class _Period>
+    constexpr std::optional<T> atomic_queue<T>::try_back_for(const std::chrono::duration<_Rep, _Period> times) {
+        if(m_consumer_lock.try_acquire_for(times)){
+            auto &res = m_data.back();
+            m_consumer_lock.release();
+            return {res};
+        }
+        else
+            return {};
+    }
+
+    template<typename T>
+    template<class _Rep, class _Period>
+    constexpr bool
+    atomic_queue<T>::try_push_for(weak_same_type<value_type> auto &&data, const std::chrono::duration<_Rep, _Period> times) {
+        if(!m_producer_lock.try_acquire_for(times))
+            return false;
+        if(!m_data.empty() && !m_consumer_lock.try_acquire_for(times)){
+            m_producer_lock.release();
+            return false;
+        }
+
+        m_data.push(data);
+        m_consumer_lock.release();
+        m_producer_lock.release();
+        return true;
+    }
+
+    template<typename T>
+    template<class _Rep, class _Period>
+    constexpr std::optional<T> atomic_queue<T>::try_pop_for(const std::chrono::duration<_Rep, _Period> times) {
+        if(!m_producer_lock.try_acquire_for(times))
+            return {};
+        if(!m_consumer_lock.try_acquire_for(times)){
+            m_producer_lock.release();
+            return {};
+        }
+
+        std::optional<T> res{std::move(m_data.front())};
+        m_data.pop();
+        if(!m_data.empty())
+            m_consumer_lock.release();
+        m_producer_lock.release();
+        return res;
     }
 
 

fad81d4ecfa71dffb688020b0b7254fc68064e8e
HWZen <1041453977@qq.com>
2022-10-16 12:45:50 +0800

    feature: add sstl.h which include all header

---
+++ include/sstl.h
@@ -0,0 +1,20 @@
+//
+// Created by HWZ on 2022/10/16.
+//
+
+#ifndef SSTL_SSTL_H
+#define SSTL_SSTL_H
+#include "algorithm.h"
+#include "array.h"
+#include "atomic_queue.h"
+#include "functional.h"
+#include "hash.h"
+#include "list.h"
+#include "pair.h"
+#include "ref_ptr.h"
+#include "RefBase.h"
+#include "sstdio.h"
+#include "thread.h"
+#include "tuple.h"
+
+#endif //SSTL_SSTL_H
